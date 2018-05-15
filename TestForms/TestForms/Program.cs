bee29cf51cff8da1c46556f5be11a5be3491cd33
manoose47 <manoose47@gmail.com>
2018-05-16 00:44:42 +0100

    Create Console

---
+++ TestForms/TestForms/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Have the properties been defined yet?");
+            Console.ReadLine();
+
+            DataModel dm = new DataModel();
+
+            // dm.Example = "The property has been set";
+
+            if (dm.Example == null)
+            {
+                Console.WriteLine("The properties haven't been set.");
+            }
+            else
+            {
+                Console.WriteLine(dm.Example);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        
+    }
+}
